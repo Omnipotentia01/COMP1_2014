3450ea45ac823ff144002d9247a89b9280169233
Tommy Cakebread <tommy_tlc@hotmail.co.uk>
2014-04-22 16:03:43 +0100

    Added new Task file and images.

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -175,6 +175,7 @@ def DisplayRecentScores(RecentScores):
   print()
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
     print("{0:<10}{1:<10}{2:<10}".format(RecentScores[Count].Name,RecentScores[Count].Score,RecentScores[Count].Date))
+  print()
   print('Press the Enter key to return to the main menu')
   input()
   print()
